c9e1e8a66ee6544bdf4e1f69ed116dca020c7f4a
agung widhiatmojo <47983775+agungwa@users.noreply.github.com>
2025-06-26 01:27:49 +0700

    Revert "fix(build): Ensure all assets are copied to dist folder"

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -24,16 +23,4 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'production',
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        { from: 'sidebar.html', to: 'sidebar.html' },
-        { from: 'app.html', to: 'app.html' },
-        { from: 'style.css', to: 'style.css' },
-        { from: 'tab-manager.css', to: 'tab-manager.css' },
-        { from: 'images', to: 'images' },
-        { from: 'manifest.json', to: 'manifest.json' },
-      ],
-    }),
-  ],
 };
